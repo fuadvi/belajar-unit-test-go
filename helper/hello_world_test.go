b71bcf46903a17e1bccef84a481a7e2c97f30902
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-05-17 17:02:49 +0700

    test skip

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,17 @@ package helper
 
 import (
 	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
 )
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can't run test on mac")
+	}
+	result := HelloWorld("fuad")
+	assert.Equal(t, "Hello fuad", result, "result must be 'hello fuad'")
+}
 
 func TestHelloWorldAssert(t *testing.T) {
 	result := HelloWorld("fuad")
